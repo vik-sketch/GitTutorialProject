7634c4fec4dc0d9370f49579d019f7474357cdb3
unknown <cena_hi8340464019@outlook.com>
2021-08-10 12:53:52 +0530

    preeti commit

---
+++ src/main/java/Pro/GitTutorialProject/Apple.java
@@ -0,0 +1,12 @@
+package Pro.GitTutorialProject;
+
+public class Apple {
+	
+	public static void main(String[] args) {
+		System.out.println("I am in Ram branch");
+		System.out.println("I am in Ram branch");
+
+	}
+	
+
+}

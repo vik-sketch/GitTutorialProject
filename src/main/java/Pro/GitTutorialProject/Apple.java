b54febd9e55aa6ace78b6c33faf25527b67b1dee
unknown <cena_hi8340464019@outlook.com>
2021-08-10 16:21:53 +0530

    my conflict preeti

--- src/main/java/Pro/GitTutorialProject/Apple.java
+++ src/main/java/Pro/GitTutorialProject/Apple.java
@@ -5,6 +5,8 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("I am in Ram branch");
 		System.out.println("I am in Ram branch");
+		System.out.println("conflict");
+
 
 	}
 	
